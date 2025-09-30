849f47533fcc1f55fb23efd7a6b7a61d5592a8d7
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-30 10:45:48 +0300

    added paths and crud methods

--- src/main/java/dev/alin/SpringNotes/controller/NoteController.java
+++ src/main/java/dev/alin/SpringNotes/controller/NoteController.java
@@ -1,11 +1,40 @@
 package dev.alin.SpringNotes.controller;
 
+import dev.alin.SpringNotes.entity.Note;
 import dev.alin.SpringNotes.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class NoteController {
     @Autowired
     private NoteService service;
+
+    @RequestMapping("notes")
+    public List<Note> getAllNotes() {
+        return service.getAllNotes();
+    }
+
+    @RequestMapping("notes/{id}")
+    public Note getNote(@PathVariable Long id) {
+        return service.getNote(id);
+    }
+
+    @PostMapping("notes")
+    public void addNote(@RequestBody Note note) {
+        service.addNote(note);
+    }
+
+    @PutMapping("notes/{id}")
+    public void updateNote(@PathVariable Long id, @RequestBody Note note) {
+        service.updateNote(id, note);
+    }
+
+    @DeleteMapping("notes/{id}")
+    public void deleteNote(@PathVariable Long id) {
+        service.deleteNote(id);
+    }
+
 }
--- src/main/java/dev/alin/SpringNotes/service/NoteService.java
+++ src/main/java/dev/alin/SpringNotes/service/NoteService.java
@@ -1,11 +1,42 @@
 package dev.alin.SpringNotes.service;
 
+import dev.alin.SpringNotes.entity.Note;
 import dev.alin.SpringNotes.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NoteService {
     @Autowired
     private NoteRepository repository;
+
+    public List<Note> getAllNotes() {
+        var list = new ArrayList<Note>();
+        repository.findAll().forEach(list::add);
+        return list;
+    }
+
+    public Note getNote(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public void addNote(Note note) {
+        repository.save(note);
+    }
+
+    public void updateNote(Long id, Note note) {
+        var tableNote = repository.findById(id).orElse(null);
+        if (tableNote != null) {
+            tableNote.setTitle(note.getTitle());
+            tableNote.setContent(note.getContent());
+            repository.save(tableNote);
+        }
+    }
+
+    public void deleteNote(Long id) {
+        repository.deleteById(id);
+    }
 }
