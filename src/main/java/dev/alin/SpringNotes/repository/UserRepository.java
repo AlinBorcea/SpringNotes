835823c9b536853544e730541fd53cf491e01af4
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-30 10:31:43 +0300

    safe deleted methods

--- src/main/java/dev/alin/SpringNotes/repository/UserRepository.java
+++ src/main/java/dev/alin/SpringNotes/repository/UserRepository.java
@@ -4,7 +4,4 @@ import dev.alin.SpringNotes.entity.User;
 import lombok.NonNull;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<@NonNull User, @NonNull Long> {
-    User findById(long id);
-    User findByName(String name);
-}
+public interface UserRepository extends CrudRepository<@NonNull User, @NonNull Long> {}
