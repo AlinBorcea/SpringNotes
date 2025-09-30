41dd3ae9b2b3b4a043b2d2d3c69e691e0fb6a00a
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-30 08:36:06 +0300

    first commit

---
+++ src/test/java/dev/alin/SpringNotes/SpringNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alin.SpringNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
