13430c6a6c7fb778b9986087710c8efadf9bac43
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-30 10:30:41 +0300

    added Table annotation and modified id field

--- src/main/java/dev/alin/SpringNotes/entity/User.java
+++ src/main/java/dev/alin/SpringNotes/entity/User.java
@@ -1,17 +1,15 @@
 package dev.alin.SpringNotes.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id = null;
 
     private String name;
 
