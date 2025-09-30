e27c73c363d6fa3911fc74eaeb0922fbeb7674a4
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-30 10:31:04 +0300

    added paths and service methods

--- src/main/java/dev/alin/SpringNotes/controller/UserController.java
+++ src/main/java/dev/alin/SpringNotes/controller/UserController.java
@@ -1,11 +1,40 @@
 package dev.alin.SpringNotes.controller;
 
+import dev.alin.SpringNotes.entity.User;
 import dev.alin.SpringNotes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService service;
+
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @RequestMapping("/users/{id}")
+    public User getUser(Long id) {
+        return service.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public void addUser(@RequestBody User user) {
+        service.addUser(user);
+    }
+
+    @PutMapping("users/{id}")
+    public void updateUser(@PathVariable Long id, @RequestBody User user) {
+        service.updateUser(id, user);
+    }
+
+    @DeleteMapping("users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+    }
+
 }
--- src/main/java/dev/alin/SpringNotes/service/UserService.java
+++ src/main/java/dev/alin/SpringNotes/service/UserService.java
@@ -1,11 +1,43 @@
 package dev.alin.SpringNotes.service;
 
+import dev.alin.SpringNotes.entity.User;
 import dev.alin.SpringNotes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
+
+    public List<User> getAllUsers() {
+        var list = new ArrayList<User>();
+        repository.findAll().forEach(list::add);
+        return list;
+    }
+
+    public User getUserById(Long id) {
+        return repository.findById(id).get();
+    }
+
+    public void addUser(User user) {
+        repository.save(user);
+    }
+
+    public void updateUser(Long id, User user) {
+        var tableUser = repository.findById(id);
+        var realUser = tableUser.orElse(null);
+
+        if (realUser != null) {
+            realUser.setName(user.getName());
+            repository.save(realUser);
+        }
+    }
+
+    public void deleteUser(Long id) {
+        repository.deleteById(id);
+    }
 }
